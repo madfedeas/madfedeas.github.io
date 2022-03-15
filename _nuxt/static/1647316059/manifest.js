7de784fa93fc1caf258f99da4d9177956a3cd6a9
madfedeas <78121411+madfedeas@users.noreply.github.com>
2022-03-14 20:50:24 -0700

    New blog domain, settled on navbar for now
    Pulling from my WordPress domain instead of madfedeas.me, removed dad's site from navbar and used bootstrap version

---
+++ _nuxt/static/1647316059/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontact","\u002F","\u002Fabout","\u002Fadditional-projects","\u002Fadditional-projects\u002Frock-paper-scissors-jquery","\u002Fadditional-projects\u002Fthe-latest-additions","\u002Fadditional-projects\u002Fthe-self-proclaimed-fan-page","\u002Fadditional-projects\u002Freactjs-and-covid-19"]})
